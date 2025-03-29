40bc43471ba6ae4ae092c526af6daa9676625f8e
tragixfn <alantragix@gmail.com>
2025-03-29 14:57:39 -0400

    Update game_server.py

--- game_server.py
+++ game_server.py
@@ -3,3 +3,16 @@ import asyncio
 import time
 
 app = FastAPI()
+clients: Dict[str, WebSocket] = {}
+turn_order = ["player1", "player2"]
+current_turn_index = 0
+current_combo = None
+combo_sender = None
+combo_start_time = None
+rounds_played = 0
+max_rounds = 4
+
+completion_times: Dict[str, List[float]] = { # Score/time of each player
+    "player1": [],
+    "player2": []
+}
